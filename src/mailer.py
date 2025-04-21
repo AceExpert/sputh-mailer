732cbe8504ec1cfc2e6ccb031ab8cdc64584ac02
sputh-the-pigeon <spuththepigeon@gmail.com>
2025-04-21 21:00:25 +0530

    cmd mailer fixes

--- src/mailer.py
+++ src/mailer.py
@@ -69,7 +69,6 @@ for em in data['to']:
 
 for domain, emails in segmented_mails.items():
     records = get_dns_record(domain, 'MX')
-    print(max(records, key = lambda rec: rec.priority).value[:-1])
-    client = smtplib.SMTP(max(records, key = lambda rec: rec.priority).value[:-1], local_hostname='sayutel.com')
+    client = smtplib.SMTP(min(records, key = lambda rec: rec.priority).value[:-1], local_hostname='sayutel.com')
     client.starttls()
     client.sendmail(data['from'], emails, composer.get_bytes())
