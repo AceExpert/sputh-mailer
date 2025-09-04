0d6f1103fb499fc15122f99ab8f3884654d51b8f
sputh-the-pigeon <spuththepigeon@gmail.com>
2025-09-04 16:37:05 +0530

    sayumail to sputh mail

--- src/mailcomposer/composer.py
+++ src/mailcomposer/composer.py
@@ -39,7 +39,7 @@ class MailComposer:
             self.html.add_header("Content-Type", "text/html", charset="UTF-8")
             self.html.add_header("Content-Transfer-Encoding", "quoted-printable")
             self.html.set_payload(quopri.encodestring(
-                ('''<div><div><div>''' + content + '''</div>''' + ('<br/><br/><div><p style="color:mediumpurple;margin:5px 0px;">Sent using SayuMail by Sayutel</p></div>' if watermark else '') + '</div></div>').encode()
+                ('''<div><div><div>''' + content + '''</div>''' + ('<br/><br/><div><p style="color:mediumpurple;margin:5px 0px;">Sent using Sputh Mail by Sayutel</p></div>' if watermark else '') + '</div></div>').encode()
             ).decode())
         self.message.attach(self.html)
         return self
